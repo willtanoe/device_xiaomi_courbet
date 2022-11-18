879a9f49ed622a9d9abe11ef69e62d460419a14e
Willtanoe <willtanu86@gmail.com>
2022-11-19 02:53:06 +0700

    awaken bringup

--- awaken_courbet.mk
+++ awaken_courbet.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common Awaken.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
-PRODUCT_NAME := spark_courbet
+PRODUCT_NAME := awaken_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
