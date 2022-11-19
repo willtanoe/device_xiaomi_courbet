b3619ac6f54688f85be9b8c57ef5c30e89c1e8b5
willtanoe <willtanu86@gmail.com>
2022-11-19 17:18:36 +0000

    Revert "P404 Bringup"
    This reverts commit 9d06496d1a07eff6bace386362b7c8216e03c6b5.

--- aosp_courbet.mk
+++ aosp_courbet.mk
@@ -11,19 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit from the 404 configuration.
-$(call inherit-product, vendor/404/configs/common.mk)
-
-# Set Boot Animination Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Include GAPPS
-WITH_GAPPS := true
-
-# Quick Taps
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := p404_courbet
+PRODUCT_NAME := aosp_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
