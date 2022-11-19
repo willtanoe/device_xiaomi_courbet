c8dee3ea9c46050c2358996275367eee3b9c72d1
willtanoe <willtanu86@gmail.com>
2022-11-19 17:18:17 +0000

    Revert "P404 Bringup"
    This reverts commit 53cc2cf16b716a0c395eb5afd588349a800900df.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,17 +1,8 @@
 #
-# Copyright (C) 2022 Project 404
+# Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
@@ -42,8 +33,6 @@ ODM_MANIFEST_COURBET_FILES := \
 # Kernel
 TARGET_KERNEL_CONFIG := courbet_defconfig
 TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CLANG_COMPILE := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
