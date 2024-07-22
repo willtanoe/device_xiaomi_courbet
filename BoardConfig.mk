courbet-space/android_device_xiaomi_courbet

courbet: Include MiuiCamera if exists
Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

BoardConfig.mk
@@ -11,6 +11,10 @@ BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/courbet
+
+# Inherit from proprietary files for miuicamera
+-include vendor/xiaomi/sweet-miuicamera/products/board.mk
+
 BUILD_BROKEN_DUP_RULES := true
 
 # Assert

device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 

willtanoe/device_xiaomi_courbet

BoardConfig.mk
@@ -7,6 +7,9 @@
 # Inherit from sm6150-common
 include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
+# Inherit from proprietary files for miuicamera
+-include vendor/xiaomi/courbet-miuicamera/products/board.mk
+
 DEVICE_PATH := device/xiaomi/courbet
 
 # Assert

device.mk
@@ -7,6 +7,9 @@
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
+# Call the MiuiCamera setup
+$(call inherit-product-if-exists, vendor/xiaomi/courbet-miuicamera/products/miuicamera.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 